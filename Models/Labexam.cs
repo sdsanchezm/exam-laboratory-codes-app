e6d8d75431adf775581d6008ff21df5de7466331
sdsanchezm <sdsanchezm@gmail.com>
2023-09-15 13:32:45 -0500

    created initial models

---
+++ Models/Labexam.cs
@@ -0,0 +1,27 @@
+﻿namespace exam_laboratory_codes_app.Models
+{
+    public class Labexam
+    {
+
+        public Guid examId { get; set; }
+        public string ExamNumber { get; set; } = "";
+        public string ExamName { get; set; } = "";
+        public string CommonName { get; set; } = "";
+        public bool FastingRequired { get; set; }
+        public string PreparationDescription { get; set; } = "";
+        public string ObservationForLaboratory { get; set; } = "";
+        public string ExamPlace { get; set; } = "";
+        public string Schedule { get; set; } = "";
+        public string ExamCode { get; set; } = "";
+        public string CupsCode { get; set; } = "";
+        public string SampleDescription { get; set; } = "";
+        public string SampleStability { get; set; } = "";
+        public string ClinicUtility { get; set; } = "";
+        public string MountingDays { get; set; } = "";
+        public string TimeResults { get; set; } = "";
+        public string SampleTechnic { get; set; } = "";
+        public string ProcessingPlace { get; set; } = "";
+
+
+    }
+}
